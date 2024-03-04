e4632eb50c8e1db3d6210efde75cd75c30f7b224
hrithikmani <hrithik.mani2000@gmail.com>
2024-03-03 22:51:52 -0500

    input-handling

---
+++ EffortHourCalculator.java
@@ -0,0 +1,15 @@
+public class EffortHourCalculator {
+    public static void main(String[] args) {
+        int sprintDays = 10; // Number of sprint days
+        int[] hoursPerDay = {6, 7, 6, 8, 7}; // Available hours per day for 5 team members
+        int hoursForCeremonies = 4; // Hours committed to sprint ceremonies by each member
+
+        System.out.println("Total Team Effort-Hour Capacity: " + calculateTeamEffortHourCapacity(sprintDays, hoursPerDay, hoursForCeremonies) + " hours");
+    }
+
+    private static int calculateTeamEffortHourCapacity(int sprintDays, int[] hoursPerDay, int hoursForCeremonies) {
+    
+      
+        return 0;
+    }
+}
