81408f4fca88ce56797d8a527323b36d87d0cec5
hrithikmani <hrithik.mani2000@gmail.com>
2024-03-03 22:35:24 -0500

    input-handling

---
+++ VelocityCalculator.java
@@ -0,0 +1,10 @@
+public class VelocityCalculator {
+    public static void main(String[] args) {
+        int[] sprintPoints = {24, 30, 28, 32, 26}; // Sample data for points completed per sprint
+        System.out.println("Average Velocity: " + calculateAverageVelocity(sprintPoints) + " points per sprint");
+    }
+
+    private static double calculateAverageVelocity(int[] sprintPoints) {
+       return 0;
+    }
+}
